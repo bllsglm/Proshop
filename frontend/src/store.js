1f005bd524f8de5fd53d66425dbd082e7217664a
bllsglm <bilalsaglam92@gmail.com>
2023-10-15 23:56:08 +0300

    create apiSlice and bring it in store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./slices/apiSlice";
+import {  getDefaultMiddleware } from "@reduxjs/toolkit/dist/getDefaultMiddleware";
+
 
 const store = configureStore({
-  reducer : {},
-});
+  reducer : {
+    [apiSlice.reducerPath] : apiSlice.reducer
+  },
+  middleware: (getDefaultMiddleware)=>getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools : true,
 
 export default store; 
