83f07b1ed39d82d5f0aebfaef8e98e0c114f0863
bllsglm <bilalsaglam92@gmail.com>
2023-10-16 23:43:36 +0300

    Build apiSlice and productApiSlice, add Loader and Message components

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,24 +1,24 @@
-import { useEffect, useState } from 'react';
+
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product'
-import axios from "axios"
-
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 const HomeScreen = () => {
 
-  const [products, setProducts] =useState([]);
 
-  useEffect(()=> {
-    const fetchData = async() => {
-      const {data} = await axios.get('/api/products');
-      setProducts(data);
-    };
-    fetchData();
-  },[])
+  const  { data: products , isLoading, isError, error} = useGetProductsQuery()
+
+
 
   return (
     <> 
-      <h1>Latest Products</h1>
+      {isLoading ? (
+        <Loader/>
+      ) : isError ? (
+      <Message variant='danger'> { error?.data?.message || error.error}</Message>) : (<>
+       <h1>Latest Products</h1>
       <Row>
         { products.map((product)=>(
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}> 
@@ -26,6 +26,8 @@ const HomeScreen = () => {
           </Col>
         ))}
       </Row>
+      </>) 
+    }     
     </>
   )
 }
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -2,79 +2,81 @@ import { useParams } from 'react-router-dom';
 import {Col, Row  ,ListGroup ,Button,Card ,Image, ListGroupItem} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Rating from '../components/Rating';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-
+import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 const ProductScreen = () => {
 
-  const [product, setProduct] = useState([]);
   const {id : productId} = useParams();
-
-  useEffect(()=> {
-    const fetchData = async() => {
-      const {data} = await axios.get(`/api/products/${productId}`)
-      setProduct(data);
-    }
-    fetchData();
-  }, [productId])
-
-
-  return <>
-     <Link className='btn btn-light my-3' to="/"> Go back</Link>
-     <Row>
-        <Col md={5}>
-          <Image src={product.image} alt={product.name} fluid/>
-        </Col>
-        <Col md={4}>
-          <ListGroup variant='flush'>
-            <ListGroupItem>
-              <h3>{product.name}</h3>
-            </ListGroupItem>
-            <ListGroupItem>
-              <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
-            </ListGroupItem>
-            <ListGroupItem>
-              Price : ${product.price}
-            </ListGroupItem>
-            <ListGroupItem>
-              Description : {product.description}
-            </ListGroupItem>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
-            <ListGroup>
-              <ListGroupItem>
-                <Row>
-                  <Col> Price: </Col>
-                  <Col>
-                    <strong>{product.price}</strong>
-                  </Col>
-                </Row>
-              </ListGroupItem>
-              <ListGroupItem>
-                <Row>
-                  <Col> Status: </Col>
-                  <Col>
-                    <strong>{product.countInStock ? "In Stock" : "Out of Stock"}</strong>
-                  </Col>
-                </Row>
-              </ListGroupItem>
-              <ListGroupItem>
-                <Button
-                  className='btn-block'
-                  type='button'
-                  disabled={product.countInStock === 0 }
-                >Add to Cart
-                </Button>
-              </ListGroupItem>
-            </ListGroup>
-          </Card>
-        </Col>
-     </Row>
-  </>
+  const  {data: product , isError, isLoading, error} = useGetProductDetailsQuery(productId)
   
+
+  return(
+  <>
+  <Link className='btn btn-light my-3' to="/"> Go back</Link>
+    {isLoading ? (<Loader/>) : isError ? ( 
+       <Message variant='danger'> 
+         { error?.data?.message || error.error}
+       </Message>) : (
+      <Row>
+         <Col md={5}>
+           <Image src={product.image} alt={product.name} fluid/>
+         </Col>
+         <Col md={4}>
+           <ListGroup variant='flush'>
+             <ListGroupItem>
+               <h3>{product.name}</h3>
+             </ListGroupItem>
+             <ListGroupItem>
+               <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+             </ListGroupItem>
+             <ListGroupItem>
+               Price : ${product.price}
+             </ListGroupItem>
+             <ListGroupItem>
+               Description : {product.description}
+             </ListGroupItem>
+           </ListGroup>
+         </Col>
+         <Col md={3}>
+           <Card>
+             <ListGroup>
+               <ListGroupItem>
+                 <Row>
+                   <Col> Price: </Col>
+                   <Col>
+                     <strong>{product.price}</strong>
+                   </Col>
+                 </Row>
+               </ListGroupItem>
+               <ListGroupItem>
+                 <Row>
+                   <Col> Status: </Col>
+                   <Col>
+                     <strong>{product.countInStock ? "In Stock" : "Out of Stock"}</strong>
+                   </Col>
+                 </Row>
+               </ListGroupItem>
+               <ListGroupItem>
+                 <Button
+                   className='btn-block'
+                   type='button'
+                   disabled={product.countInStock === 0 }
+                 >Add to Cart
+                 </Button>
+               </ListGroupItem>
+             </ListGroup>
+           </Card>
+         </Col>
+      </Row>
+   
+   )
+   }
+   </>
+  )
+
+
 }
 
 export default ProductScreen
