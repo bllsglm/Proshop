d8f498b63dcade3e6357068817fce5d0448b3489
bllsglm <bilalsaglam92@gmail.com>
2023-10-26 02:32:31 +0300

    Upload Product Image with Multer

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -27,5 +27,50 @@ const getProductById = asyncHandler(async(req,res) => {
   }
 })
 
+// desc Create a Product
+// route  POST /api/products
+// @access Private/Admin
+const createProduct = asyncHandler(async(req,res) => {
+  const product = new Product({
+    name : 'Sample name',
+    price : 0,
+    user : req.user._id,
+    image : '/images/sample.jpg',
+    brand : 'sample brand',
+    category: "sample category",
+    countInstock : 0,
+    numReviews:0,
+    description: 'sample description',
+  });
 
-export {getProducts , getProductById}
+  const createdProduct = await product.save();
+  res.status(200).json(createdProduct);
+})
+
+
+// desc Update a Product
+// route  PUT /api/products/:id
+// @access Private/Admin
+const updateProducts = asyncHandler(async(req,res) => {
+  const { name, price ,description, image , brand, category, countInstock} =req.body;
+  const product = await Product.findById(req.params.id);
+
+  if(product){
+    product.name = name
+    product.price = price 
+    product.description = description 
+    product.image = image 
+    product.brand = brand 
+    product.category = category 
+    product.countInstock = countInstock 
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  }else{
+    res.status(404);
+    throw new Error('Resource not found')
+  }
+})
+
+
+export {getProducts , getProductById, createProduct ,updateProducts}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,14 +1,13 @@
 import express from "express";
 const router = express.Router();
-import { getProductById, getProducts } from "../controllers/productController.js";
+import { getProductById, getProducts, createProduct, updateProducts } from "../controllers/productController.js";
+import {protect, admin} from "../middleware/authMiddleware.js";
 
-
-router.route("/").get(getProducts)
-router.route("/:id").get(getProductById)
-
+router.route("/").get(getProducts).post(protect ,admin ,createProduct)
+router.route("/:id").get(getProductById).put(protect, admin, updateProducts)
 
 export default router
-
+ 
 
 
 
